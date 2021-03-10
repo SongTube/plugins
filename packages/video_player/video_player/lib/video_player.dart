3fccabfae8ee1a86565f1cc29af2b8d4adfef2fd
Jesus Acosta <jesusgabriel.91@gmail.com>
2021-03-09 23:54:35 -0400

    temporarily disable AppLifecycleState.paused

--- packages/video_player/video_player/lib/video_player.dart
+++ packages/video_player/video_player/lib/video_player.dart
@@ -578,8 +578,6 @@ class _VideoAppLifeCycleObserver extends Object with WidgetsBindingObserver {
   void didChangeAppLifecycleState(AppLifecycleState state) {
     switch (state) {
       case AppLifecycleState.paused:
-        _wasPlayingBeforePause = _controller.value.isPlaying;
-        _controller.pause();
         break;
       case AppLifecycleState.resumed:
         if (_wasPlayingBeforePause) {
